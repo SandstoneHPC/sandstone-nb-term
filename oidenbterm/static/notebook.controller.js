3e2b807048c3115a7509dbf8166894a0c57850cf
saurabhsood91 <saurabhsood91@gmail.com>
2016-02-26 13:38:00 -0700

    initial commit of backend api

--- oidenbterm/static/notebook.controller.js
+++ oidenbterm/static/notebook.controller.js
@@ -2,22 +2,21 @@
 
 angular.module('oide.nbterm')
 
-.controller('NotebookCtrl', [function() {
+.controller('NotebookCtrl', ['$http', function($http) {
   var self = this;
 
-  self.cells = [
-    {
-      type:'code',
-      isActive:true,
-    },{
-      type:'code'
-    },{
-      type:'markdown'
-    },{
-      type:'markdown',
-      input: '__HI__ this is a *test*!!'
-    },{
-      type:'markdown',
-      input: '__HI__ this is __another__ a *test*!!'
-    }];
+  self.cells = [];
+
+  // self.executeCode = function() {
+  //   $http({
+  //     url: '/nbterm/a/kernel/execute',
+  //     method: 'POST',
+  //     params: {
+  //       _xsrf:getCookie('_xsrf'),
+  //       code: "print 'Hello, World'"
+  //     }
+  //   }).success(function(data){
+  //     console.log(data);
+  //   });
+  // }
 }]);
--- oidenbterm/urls.py
+++ oidenbterm/urls.py
@@ -1,10 +1,12 @@
 from terminado import SingleTermManager
 from oidenbterm.handlers import AuthTermSocket
+from oidenbterm.handlers import KernelHandler
 
 
 term_manager = SingleTermManager(shell_command=['bash'])
 
 URL_SCHEMA = [
             (r"/nbterm/a/term", AuthTermSocket,
-                    {'term_manager': term_manager})
+                    {'term_manager': term_manager}),
+            (r"/nbterm/a/kernel/execute", KernelHandler)
         ]
