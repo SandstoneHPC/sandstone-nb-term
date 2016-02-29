c6f68851263a81027a386dee553c150d541ee705
Saurabh Sood <saurabh.sood@colorado.edu>
2016-02-29 14:01:28 -0700

    add shutdown kernel method

--- oidenbterm/static/notebook.controller.js
+++ oidenbterm/static/notebook.controller.js
@@ -7,16 +7,34 @@ angular.module('oide.nbterm')
 
   self.cells = [];
 
-  // self.executeCode = function() {
-  //   $http({
-  //     url: '/nbterm/a/kernel/execute',
-  //     method: 'POST',
-  //     params: {
-  //       _xsrf:getCookie('_xsrf'),
-  //       code: "print 'Hello, World'"
-  //     }
-  //   }).success(function(data){
-  //     console.log(data);
-  //   });
-  // }
+  self.executeCode = function() {
+    $http({
+      url: '/nbterm/a/kernel/execute',
+      method: 'POST',
+      data: {
+        code: "print 'Hello, World'",
+        operation: 'EXECUTE_CODE'
+      },
+      params: {
+        _xsrf:getCookie('_xsrf')
+      }
+    }).success(function(data){
+      console.log(data);
+    });
+  };
+  
+  self.shutdownKernel = function() {
+    $http({
+      url: '/nbterm/a/kernel/execute',
+      method: 'POST',
+      data: {
+        operation: 'SHUTDOWN_KERNEL'
+      },
+      params: {
+        _xsrf:getCookie('_xsrf')
+      }
+    }).success(function(data){
+      console.log(data);
+    });
+  };
 }]);
