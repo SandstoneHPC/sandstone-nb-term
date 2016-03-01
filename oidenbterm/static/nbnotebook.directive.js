9ecba99cabf360a335fe3d8c02354e36ce894a67
Zeb Sampedro <sampedro@colorado.edu>
2016-03-01 12:02:42 -0700

    Most functionality stubbed in for directive.

--- oidenbterm/static/nbnotebook.directive.js
+++ oidenbterm/static/nbnotebook.directive.js
@@ -7,7 +7,8 @@ angular.module('oide.nbterm')
     restrict: 'A',
     scope: {
       kernelName: '@',
-      cells: '='
+      cells: '=',
+      kernelStatus: '='
     },
     templateUrl: '/static/nbterm/templates/nb-notebook.html',
     controller: function($scope,$element) {
@@ -16,6 +17,41 @@ angular.module('oide.nbterm')
         {id:'code',name:'Code'},
         {id:'heading',name:'Heading'}
       ];
+      $scope.setActive = function(cell) {
+        angular.forEach($scope.cells, function(c) {
+          c.isActive = false;
+        });
+        cell.isActive = true;
+      };
+      $scope.deleteCell = function(cell) {
+        var index = $scope.cells.indexOf(cell);
+        $scope.cells.splice(index,1);
+      };
+      $scope.insertCellAbove = function(cell) {
+        var index = $scope.cells.indexOf(cell);
+        var newCell = createNewCell('code');
+        $scope.cells.splice(index-1, 0 , newCell);
+        $scope.setActive(newCell);
+      };
+      $scope.insertCellBelow = function(cell) {
+        var index = $scope.cells.indexOf(cell);
+        var newCell = createNewCell('code');
+        $scope.cells.splice(index+1, 0 , newCell);
+        $scope.setActive(newCell);
+      };
+      $scope.runCell = function(cell) {
+        $scope.kernelStatus = 'busy';
+        // Eventually, the nb service will set the output
+        // on the model, and then revert its status to 'idle'.
+        $scope.kernelStatus = 'idle';
+        cell.hasExecuted = true;
+        cell.output = 'This code ran!';
+      };
+      var createNewCell = function(cellType) {
+        return {
+          type: cellType
+        };
+      };
     }
   };
 }]);
--- oidenbterm/static/notebook.controller.js
+++ oidenbterm/static/notebook.controller.js
@@ -9,19 +9,15 @@ angular.module('oide.nbterm')
     {
       type:'code',
       isActive:true,
-      hasExecuted:true
     },{
       type:'code'
     },{
       type:'markdown'
     },{
       type:'markdown',
-      hasExecuted: true,
       input: '__HI__ this is a *test*!!'
     },{
       type:'markdown',
-      isActive: true,
-      hasExecuted: true,
       input: '__HI__ this is __another__ a *test*!!'
     }];
 }]);
