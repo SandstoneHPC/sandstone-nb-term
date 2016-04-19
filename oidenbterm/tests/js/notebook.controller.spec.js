8c42faed23061302c44f765216c6ddfa546a2c70
saurabhsood91 <saurabhsood91@gmail.com>
2016-04-19 16:10:19 -0600

    bootstrap tests

---
+++ oidenbterm/tests/js/notebook.controller.spec.js
@@ -0,0 +1,47 @@
+describe('bashnotebook', function(){
+
+  var controller;
+  var httpBackend;
+  var $compile;
+  var nbService;
+
+  beforeEach(module('oide'));
+  beforeEach(module('oide.editor'));
+  beforeEach(module('oide.filesystemservice'));
+  beforeEach(module('oide.templates'));
+  beforeEach(module('oide.filetreedirective'));
+
+  beforeEach(inject(function($controller, $rootScope, $log, $document, $httpBackend, _$compile_, $modal, NotebookService){
+    scope = $rootScope.$new();
+    $compile = _$compile_;
+    httpBackend = $httpBackend;
+    nbService = NotebookService;
+    httpBackend.whenGET('/filebrowser/filetree/a/dir').respond(function(){
+      return [200, dirs];
+    });
+    controller = $controller;
+    controller = $controller('NotebookCtrl', {
+      $scope: scope,
+      $document: $document,
+      $log: $log,
+      NotebookService: NotebookService,
+      $modal: $modal
+    });
+    scope.ctrl = controller;
+    scope.$apply();
+  }));
+
+  describe('Notebook Controller', function(){
+    it('should initialize the directive properly', function(){
+      var element = angular.element('<div oide-filetree tree-data="ctrl.treeData" leaf-level="file" selection-desc="ctrl.sd"></div>');
+      el = $compile(element)(scope);
+      scope.$digest();
+      expect(scope.ctrl.cellsObj).toBeDefined();
+      expect(scope.ctrl.nbFile).toBeDefined();
+      expect(scope.ctrl.nbFile.filepath).toBe('-');
+      expect(scope.ctrl.nbFile.filename).toBe('Untitled');
+      expect(scope.ctrl.unsaved).not.toBeTruthy();
+    });
+  });
+
+});
