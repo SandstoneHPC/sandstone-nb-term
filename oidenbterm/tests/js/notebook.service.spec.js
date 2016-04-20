4414efa6f972e87b3a62162a9d522cb316fd7fcb
saurabhsood91 <saurabhsood91@gmail.com>
2016-04-20 14:06:10 -0600

    add specs to stop and start kernel

--- oidenbterm/tests/js/notebook.service.spec.js
+++ oidenbterm/tests/js/notebook.service.spec.js
@@ -11,12 +11,27 @@ describe('NotebookService', function(){
   beforeEach(inject(function(NotebookService, $httpBackend){
     nbService = NotebookService;
     httpBackend = $httpBackend;
+
+    httpBackend.whenPOST('/nbterm/a/kernel/execute').respond(function(){
+      return [200, {'result': 'kernel_status changed'}];
+    });
+
   }));
 
   describe('NotebookService tests', function(){
     it('should should initialize properly', function(){
       expect(nbService.cellsObj).toBeDefined();
       expect(nbService.getKernelStatus()).toBe('idle');
+    });
+    it('should start the kernel and set the kernel status to idle', function(){
+      nbService.startKernel();
+      httpBackend.flush();
+      expect(nbService.getKernelStatus()).toBe('idle');
+    });
+    it('should stop the kernel and set the kernel status to stopped', function(){
+      nbService.stopKernel();
+      httpBackend.flush();
+      expect(nbService.getKernelStatus()).toBe('stopped');
     });
   });
 
