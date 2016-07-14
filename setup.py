626935d3693072b34af73fe24ec765c34d7cfb98
Zebula Sampedro <sampedro@colorado.edu>
2016-07-14 13:42:57 -0600

    Bumped version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='sandstone-nb-term',
-    version='0.1.1',
+    version='0.1.2',
     author=u'Zebula Sampedro',
     author_email='sampedro@colorado.edu',
     packages=find_packages(),
@@ -15,7 +15,7 @@ setup(
     long_description=open('DESCRIPTION.rst').read(),
     zip_safe=False,
     install_requires=[
-        'sandstone',
+        'sandstone==0.12.5',
         'jupyter',
         'bash_kernel',
     ],
