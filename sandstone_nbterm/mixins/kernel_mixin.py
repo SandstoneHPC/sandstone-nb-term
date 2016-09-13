483a2fd7b4819ca6e6b17dbb38949ccfd2f59c70
Zebula Sampedro <sampedro@colorado.edu>
2016-09-13 16:02:19 -0600

    Fixed kernel spawning issue.

--- sandstone_nbterm/mixins/kernel_mixin.py
+++ sandstone_nbterm/mixins/kernel_mixin.py
@@ -5,26 +5,24 @@ from sandstone import settings
 class KernelMixin(tornado.web.RequestHandler):
     def initialize(self, kernel_name='python2'):
         self.kernel_name = kernel_name
-        self.kernel_started = False
         if not hasattr(self.application, 'kernel'):
+            self.kernel_started = False
             self.start_kernel()
         else:
+            self.kernel_started = True
             self.kernel = self.application.kernel
             self.kernel_manager = self.application.kernel_manager
 
     def start_kernel(self):
-        # if not hasattr(self.application, 'kernel'):
-        self.kernel_manager = jupyter_client.KernelManager()
-        self.kernel_manager.kernel_name = self.kernel_name
-        self.kernel_manager.start_kernel()
-        self.application.kernel_manager = self.kernel_manager
-        self.application.kernel = self.kernel_manager.client()
-        self.application.kernel.start_channels()
-        self.kernel = self.application.kernel
-        self.kernel_started = True
-        # else:
-        #     self.kernel_manager.start_kernel()
-        #     self.kernel.start_channels()
+        if not hasattr(self.application, 'kernel'):
+            self.kernel_manager = jupyter_client.KernelManager()
+            self.kernel_manager.kernel_name = self.kernel_name
+            self.kernel_manager.start_kernel()
+            self.application.kernel_manager = self.kernel_manager
+            self.application.kernel = self.kernel_manager.client()
+            self.application.kernel.start_channels()
+            self.kernel = self.application.kernel
+            self.kernel_started = True
 
     def shutdown_kernel(self):
         # shutdown the kernel
